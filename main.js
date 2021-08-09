8075065b7b9938c01a6d4c6a5cd50026e5918037
dontexpectmuch <pauldstotts@gmail.com>
2021-08-09 13:01:38 -0500

    pds

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 "use strict";
 
 //This is my primary JS site
+
+request.get("https://username.carto.com/endpoint/", {
+  auth: {
+    user: "username",
+    pass: 1234567890123456789012345678901234567890,
+  },
+});
+
+//Use Postman to authenticate
