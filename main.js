52bb59d7c26bf36d2fdda99784a4daaf248d8122
dontexpectmuch <pauldstotts@gmail.com>
2021-08-17 12:36:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 
 //This is my primary JS site
 
-request.get("https://username.carto.com/endpoint/", {
-  auth: {
-    user: "username",
-    pass: 1234567890123456789012345678901234567890,
-  },
-});
+// request.get("https://username.carto.com/endpoint/", {
+//   auth: {
+//     user: "username",
+//     pass: 1234567890123456789012345678901234567890,
+//   },
+// });
 
 //Use Postman to authenticate
+
+
+console.log(document.querySelector(`.news-letter`).textContent = `Replaced newsletter`);
